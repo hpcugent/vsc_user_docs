809a87e61933b31c747841aa37b446e2b4d35ed2
Toon Willems <willemstoon@gmail.com>
2014-03-03 10:21:36 +0100

    abstract away site specific thingies

--- filter.hs
+++ filter.hs
@@ -5,7 +5,14 @@ import Text.Pandoc
 
 import Debug.Trace
 import System.Environment
+import System.FilePath
 
+
+filename True basename = do
+  siteDir <- getEnv "VSC_SITE"
+  return $ siteDir </> basename
+
+filename False basename = return basename
 
 inline :: Block -> IO [Block]
 inline h@(Header n attr text) = do
@@ -13,17 +20,25 @@ inline h@(Header n attr text) = do
     Just v -> include v
     Nothing -> return [h]
   where
-    (identifiers, classes, variables) = attr
+    (identifier, classes, variables) = attr
+    isSpecific = "specific" == identifier
+
     include f = do
-      siteDir <- getEnv "VSC_SITE"
-      string <- readFile (siteDir ++ "/" ++ f)
+      filename <- filename isSpecific f
+      string <- readFile filename
       (Pandoc meta blocks) <- return $ readMarkdown def string
       return blocks
 
-inline cb@(CodeBlock (id, classes, namevals) contents) = do
-  case lookup "basename" namevals of
-      Just f     -> return . return . (CodeBlock (id, classes, namevals)) =<< readFile f
+inline cb@(CodeBlock (identifier, classes, variables) contents) = do
+    case lookup "basename" variables of
+      Just f     -> return . return . (CodeBlock (identifier, classes, variables)) =<< readFile' f
       Nothing    -> return [cb]
+  where
+    isSpecific = "specific" == identifier
+    readFile' f = do
+      name <- filename isSpecific f
+      readFile name
+
 
 inline x = return [x]
 
