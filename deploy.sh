8f74dba96dbd740c5cfbfb5dae55889e322c4909
Ward Poelmans <wpoely86@gmail.com>
2016-12-06 10:49:40 +0100

    deployscript: put pdf in seperate directory
    And use UGent address for deploy commits

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ set -e
 echo "Preparing to deploy"
 
 cd $HOME/build
-ls
+mkdir pdf
+mv *.pdf pdf
 
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
+git config --global user.email "hpc@ugent.be"
+git config --global user.name "HPC UGent"
 
 git init
-git add ./*.pdf
+git add ./pdf/*.pdf
 git status
 git commit -m "Deploy to Github Pages"
-git log
 git push --force "https://${GH_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
