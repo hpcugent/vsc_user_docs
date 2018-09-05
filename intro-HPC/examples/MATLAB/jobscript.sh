534621f8fe34a27e15bf4f24f4de3bf52741d076
redfast00 <redfast00@gmail.com>
2018-09-05 15:46:53 +0200

    More changes

--- intro-HPC/examples/MATLAB/jobscript.sh
+++ intro-HPC/examples/MATLAB/jobscript.sh
@@ -1,61 +1,26 @@
 #!/bin/bash
-
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=1:00:00
+#PBS -l walltime=1:0:0
+#
+# Example (single-core) MATLAB job script
+# see http://hpcugent.github.io/vsc_user_docs/
+#
 
-## name of the executable
-name=magicsquare
-## arguments to pass to the executable ("" is no options)
-opts="5"
-## directory where the executable and script can be found
-## PBS_O_WORKDIR variable points to the directory you are submitting from
-dir=$PBS_O_WORKDIR
-#dir=/path/to/dir
+# make sure the MATLAB version matches with the one used to compile the MATLAB program!
+module load MATLAB/2018a
 
-## version: version of MATLAB
-version=2018a
+# use temporary directory (not $HOME) for (mostly useless) MATLAB log files
+# subdir in $TMPDIR (if defined, or /tmp otherwise)
+export MATLAB_LOG_DIR=$(mktemp -d -p  ${TMPDIR:-/tmp})
 
-## Size of MATLAB cache in memory
-cache_size=1024M
-#################################################################
-###  Don't change things below unless you know what you are doing
-#################################################################
+# configure MATLAB Compiler Runtime cache location & size (1GB)
+# use a temporary directory in /dev/shm (i.e. in memory) for performance reasons
+export MCR_CACHE_ROOT=$(mktemp -d -p /dev/shm)
+export MCR_CACHE_SIZE=1024MB
 
-module load MATLAB/${version}
+# change to directory where job script was submitted from
+cd $PBS_O_WORKDIR
 
-if [ ! -d $dir ]
-then
-  echo "Directory $dir is not a directory"
-  exit 1
-fi
-
-cd $dir
-
-if [ ! -x $name ]
-then
-  echo "No executable $name found."
-  exit 2
-fi
-script=run_${name}.sh
-if [ ! -x  $script ]
-then
-  echo "No run script $script found"
-  exit 3
-fi
-
-## make cache dir
-## /dev/shm is memory, so this should be really fast
-
-cdir=$(mktemp -d -p /dev/shm)
-if [ ! -d $cdir ]
-then
-  echo "No tempdir $cdir found."
-  exit 1
-fi
-
-## set dir
-export MCR_CACHE_ROOT=$cdir
-## 1GB cache (more then large enough)
-export MCR_CACHE_SIZE=$cache_size
-## real running
-./$script $EBROOTMATLAB $opts
+# run compiled example MATLAB program 'magicsquare', provide '5' as argument
+# $EBROOTMATLAB points to MATLAB installation directory
+./run_magicsquare.sh $EBROOTMATLAB 5
