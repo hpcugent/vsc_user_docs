1af1dc78435e8341c00abb7f846bd133693259ff
redfast00 <redfast00@gmail.com>
2018-09-07 16:28:47 +0200

    Script to add timestamp when deploying

---
+++ add-timestamp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e -u
+
+function set_timestamp() {
+    directory=$1;
+    last_modified=$(git log -n 1 --pretty='format:%cd' --date=format:'%d.%m.%Y' "$1")
+    echo "${last_modified}"
+    sed --in-place "s/DATEPLACEHOLDER/${last_modified}/g" "$1/title.tex"
+}
+
+set_timestamp perfexpert
+set_timestamp intro-Linux
+set_timestamp intro-HPC
