764171251b9f400f4ae31c1a126a73faf3bcb169
redfast00 <redfast00@gmail.com>
2018-09-10 09:05:51 +0200

    Change date format in order not to be ambiguous

--- add-timestamp.sh
+++ add-timestamp.sh
@@ -4,7 +4,7 @@ set -e -u
 
 function set_timestamp() {
     directory=$1;
-    last_modified=$(git log -n 1 --pretty='format:%cd' --date=format:'%d.%m.%Y' "$1")
+    last_modified=$(git log -n 1 --pretty='format:%cd' --date=format:'%B %e %Y' "$1")
     echo "${last_modified}"
     sed --in-place "s/DATEPLACEHOLDER/${last_modified}/g" "$1/title.tex"
 }
