5572c422775b162b02a4a3cde52eaf7917c118e3
redfast00 <redfast00@gmail.com>
2018-09-18 15:21:34 +0200

    More changes

--- intro-HPC/examples/Job-script-examples/timeout.sh
+++ intro-HPC/examples/Job-script-examples/timeout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N timeout_example
-#PBS -l nodes=1:ppn=1         ## single-node job, single core
-#PBS -l walltime=20:00:00     ## max. 20h of wall time
+#PBS -l nodes=1:ppn=1        ## single-node job, single core
+#PBS -l walltime=2:00:00     ## max. 2h of wall time
 
 # go to temporary working directory (on local disk)
 cd $TMPDIR
@@ -9,9 +9,10 @@ cd $TMPDIR
 # $PBS_O_WORKDIR/example_program.sh 1400 output.txt
 
 # So we put it after a timeout command
-# We have a total of 60 * 20 minutes (=1200 minutes), but we only give it
-#  1150 minutes, so we have 50 minutes left to copy files back. This should
+# We have a total of 120 minutes (2 x 60) and we instruct the script to run for
+# 100 minutes, but timeout after 90 minute,
+# so we have 30 minutes left to copy files back. This should
 #  be more than enough.
-timeout -s SIGKILL 1150m $PBS_O_WORKDIR/example_program.sh 1400 output.txt
+timeout -s SIGKILL 90m $PBS_O_WORKDIR/example_program.sh 100 output.txt
 # copy back output data, ensure unique filename using $PBS_JOBID
 cp output.txt $VSC_DATA/output_${PBS_JOBID}.txt
