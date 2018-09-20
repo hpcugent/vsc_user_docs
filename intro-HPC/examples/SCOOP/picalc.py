fc24b04fa6a7ba4c3251a38d92f01b0ff975eac9
redfast00 <redfast00@gmail.com>
2018-09-20 13:27:01 +0200

    Scoop

---
+++ intro-HPC/examples/SCOOP/picalc.py
@@ -0,0 +1,38 @@
+from math import hypot
+from random import random
+from scoop import futures
+
+NAME = 'SCOOP_piCalc'
+
+# A range is used in this function for python3. If you are using python2,
+# an xrange might be more efficient.
+try:
+    range_fn = xrange
+except:
+    range_fn = range
+
+
+def test(tries):
+    return sum(hypot(random(), random()) < 1 for i in range_fn(tries))
+
+# Calculates pi with a Monte-Carlo method. This function calls the function
+# test "n" times with an argument of "t". Scoop dispatches these
+# functions interactively accross the available resources.
+def calcPi(workers, tries):
+    expr = futures.map(test, [tries] * workers)
+    piValue = 4. * sum(expr) / float(workers * tries)
+    return piValue
+
+
+if __name__ == '__main__':
+    import sys
+    nr_batches = 3000
+    batch_size = 5000
+
+    try:
+        nr_batches = int(sys.argv[1])
+        batch_size = int(sys.argv[2])
+    except:
+        pass
+
+    print "PI=%f (in nr_batches=%d,batch_size=%d)" % (calcPi(nr_batches, batch_size), nr_batches, batch_size)
