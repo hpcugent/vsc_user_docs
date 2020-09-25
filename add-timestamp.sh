fb075fe3d3cee79a1761a0f85af772e247fdf3f6
sam <sammoors@gmail.com>
2020-09-25 11:07:29 +0200

    update timestamp

--- add-timestamp.sh
+++ add-timestamp.sh
@@ -5,7 +5,7 @@ set -e -u
 function set_timestamp() {
     directory=$1;
     last_modified=$(git log -n 1 --pretty='format:%cd' --date=format:'%B %e %Y' "$1")
-    echo "${last_modified}"
+    echo "$1 ${last_modified}"
     sed --in-place "s/DATEPLACEHOLDER/${last_modified}/g" "$1/title.tex"
 }
 
