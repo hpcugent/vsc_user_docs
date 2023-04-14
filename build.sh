993738c199855c8fa62846af60ec890fd896481b
stdweird <stijn.deweirdt@ugent.be>
2023-04-14 10:39:44 +0200

    fix buildscript

--- build.sh
+++ build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-# python3 -m pip install --ignore-installed --prefix $PWD/pypkgs -r requirements.txt
-# python3 -m pip install --prefix $PWD/pypkgs $PWD/custom_plugin
-# python3 -m pip install --prefix $PWD/pypkgs $PWD/computational_macros
-PYTHONPATH=$PWD/pypkgs/lib/python3.9/site-packages:$PWD/custom_plugin:$PWD/computational_macros:$PYTHONPATH python3 build.py
+set -x
+if [ ${INSTALLDEPS:-0} -gt 0 ]; then
+    python3 -m pip install --ignore-installed --prefix $PWD/pypkgs -r requirements.txt
+    python3 -m pip install --prefix $PWD/pypkgs $PWD/custom_plugin
+    python3 -m pip install --prefix $PWD/pypkgs $PWD/computational_macros
+fi
+
+pyver=python$(python3 -c "import sys; print(\"%s.%s\" % sys.version_info[:2])")
+export PYTHONPATH=$PWD/pypkgs/lib/$pyver/site-packages:$PWD/pypkgs/lib64/$pyver/site-packages:$PWD/custom_plugin:$PWD/computational_macros:$PYTHONPATH
+export PATH=$PWD/pypkgs/bin:$PATH
+
+python3 build.py "$@"
+
+if [ ${WEBSERVER:-0} -gt 0 ]; then
+    cd build/HPC
+    port=8000
+    {
+        sleep 1
+        if command -v xdg-open >& /dev/null; then
+            xdg-open http://localhost:$port
+            echo "Tab opened for http://localhost:$port"
+        else
+            echo "Go to http://localhost:$port"
+        fi
+    } &
+    python -m http.server --cgi 8000
+fi
