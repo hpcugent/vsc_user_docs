ac145f0d222656ff92bf12c674c3e45a21069367
stdweird <stijn.deweirdt@ugent.be>
2023-04-14 14:41:38 +0200

    add javascript to let OS picker pick based on previous choice. this is     POC, it now runs after displaying the page, so it causes a short flash.

--- custom_plugin/constants.py
+++ custom_plugin/constants.py
@@ -16,16 +16,71 @@ OS_PICK_BTN = """
 """
 
 # This string enables automatic scrolling to section on some page while using OS picking feature.
+# localStorage with expiration is from https://www.sohamkamani.com/javascript/localstorage-with-ttl-expiry/
 JS_SCROLL_STR = """
 <script>
-  var buttons = Array.from(document.getElementsByClassName("md-button"))
-  buttons.forEach(
-          function (btn) {
+
+    function setWithExpiry(key, value, ttl) {
+        const now = new Date()
+
+        // `item` is an object which contains the original value
+        // as well as the time when it's supposed to expire
+        const item = {
+            value: value,
+            expiry: now.getTime() + ttl,
+        }
+        localStorage.setItem(key, JSON.stringify(item))
+    }
+
+    function getWithExpiry(key) {
+        const itemStr = localStorage.getItem(key)
+        // if the item doesn't exist, return null
+        if (!itemStr) {
+            return null
+        }
+        const item = JSON.parse(itemStr)
+        const now = new Date()
+        // compare the expiry time of the item with the current time
+        if (now.getTime() > item.expiry) {
+            // If the item is expired, delete the item from storage
+            // and return null
+            localStorage.removeItem(key)
+            return null
+        }
+        return item.value
+    }
+
+    function match_OS(href) {
+        // absolute url
+        const osmatch = href.match(/\/(Linux|macOS|Windows)\//i)
+        if (osmatch !== null) {
+            return osmatch[1]
+        }
+        return null
+    }
+
+    const store_OS = getWithExpiry("os-pick-OS");
+
+    var buttons = Array.from(document.getElementsByClassName("md-button"))
+    buttons.forEach(
+        function (btn) {
+            const osmatchbtn = match_OS(btn.href)
+            if (osmatchbtn !== null) {
+                //console.log("match button OS", osmatchbtn, "store_OS", store_OS)
+                if (osmatchbtn == store_OS) {
+                    //console.log("next go to ", btn.href + "#" + window.location.hash.substring(1))
+                    window.location.href = btn.href + "#" + window.location.hash.substring(1)
+                }
+            }
             btn.addEventListener("click", function () {
-                var hash = window.location.hash.substring(1);
-                this.href = this.href + "#" + hash
+                const osmatch = match_OS(this.href)
+                if (osmatch !== null) {
+                    //console.log("match this button OS onclick", osmatch)
+                    setWithExpiry("os-pick-OS", osmatch, 12*3600*1000)  // TTL in ms
+                }
+                this.href = this.href + "#" + window.location.hash.substring(1)
             })
-          }
-  )
+        }
+    )
 </script>
 """
