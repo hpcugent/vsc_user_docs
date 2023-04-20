460a2710a0ae050999135a0c7ad291a5a32da5b7
stdweird <stijn.deweirdt@ugent.be>
2023-04-20 14:15:46 +0200

    initial linkchecker support, but needs more testing

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ if [ "${INSTALLDEPS:-0}" -gt 0 ]; then
     python3 -m pip install --ignore-installed --prefix "$DEPSDIR" -r requirements.txt
     python3 -m pip install --prefix "$DEPSDIR" "$PWD"/custom_plugin
     python3 -m pip install --prefix "$DEPSDIR" "$PWD"/computational_macros
+    # not needed for actual page
+    python3 -m pip install --ignore-installed --prefix "$DEPSDIR" linkchecker
 fi
 
 pyver=python$(python3 -c "import sys; print(\"%s.%s\" % sys.version_info[:2])")
@@ -17,15 +19,19 @@ if [ "${WEBSERVER:-0}" -gt 0 ]; then
     webbase=build
     if cd "$webbase"; then
         port=8000
+        url=http://localhost:$port
         {
             sleep 1
             if command -v xdg-open >& /dev/null; then
-                xdg-open http://localhost:$port
-                echo "Tab opened for http://localhost:$port"
+                xdg-open $url
+                echo "Tab opened for $url"
             else
-                echo "Go to http://localhost:$port"
+                echo "Go to $url"
             fi
             echo "To have working legacy pdf links, also do a 'cp -a pdf build'"
+
+            # run tests
+            linkchecker $url
         } &
         python -m http.server --cgi 8000
     else
