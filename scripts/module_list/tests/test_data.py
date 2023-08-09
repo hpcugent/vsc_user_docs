8594510e11edea6a8b5b0be0e35b72059571019b
Michiel Lachaert <michiellachaert@outlook.com>
2023-08-09 14:14:44 +0200

    update tests

--- scripts/module_list/tests/test_data.py
+++ scripts/module_list/tests/test_data.py
@@ -17,25 +17,9 @@ class TestData:
     # ---------------------------
 
     def test_data_ugent(self):
-        os.environ["MOCK_FILE_AVAIL"] = self.path + "/data/data_avail_simple.txt"
+        os.environ["MOCK_FILE_AVAIL"] = self.path + "/data/data_avail_simple_pikachu.txt"
         os.environ["MOCK_FILE_AVAIL_CLUSTER"] = self.path + "/data/data_avail_cluster_simple.txt"
         sol = modules_ugent()
         assert len(sol) == 2
-        assert len(sol["cluster/victini"]) == 15
-        assert list(simplify_modules(sol["cluster/victini"])) == ["ABAQUS", "ABINIT", "zstd"]
-
-    def test_clusters_ugent(self):
-        os.environ["MOCK_FILE_AVAIL_CLUSTER"] = self.path + "/data/doduo_avail_cluster.txt"
-        sol = clusters_ugent()
-        assert len(sol) == 8
-        assert list(sol) == ['cluster/accelgor', 'cluster/doduo', 'cluster/donphan',
-                             'cluster/gallade', 'cluster/joltik', 'cluster/skitty',
-                             'cluster/swalot', 'cluster/victini']
-
-    def test_large(self):
-        os.environ["MOCK_FILE_AVAIL"] = self.path + "/data/doduo_avail.txt"
-        os.environ["MOCK_FILE_AVAIL_CLUSTER"] = self.path + "/data/doduo_avail_cluster.txt"
-        sol = modules_ugent()
-        assert len(sol) == 8
-        assert len(sol["cluster/victini"]) == 5504
-        assert len(simplify_modules(sol["cluster/victini"])) == 1391
+        assert len(sol["cluster/dialga"]) == 15
+        assert list(simplify_modules(sol["cluster/dialga"])) == ["cfd", "llm", "science"]
--- scripts/module_list/tests/test_module.py
+++ scripts/module_list/tests/test_module.py
@@ -1,4 +1,5 @@
 import os
+from module_overview import module_avail, filter_fn_gent_modules, filter_fn_gent_cluster
 
 
 class TestModule:
@@ -9,16 +10,34 @@ class TestModule:
 
     @classmethod
     def setup_class(cls):
-        os.environ["LMOD_CMD"] = cls.path + "data/lmod_mock.sh"
+        os.environ["LMOD_CMD"] = cls.path + "/data/lmod_mock.sh"
 
     # ---------------------------
     # Module tests
     # ---------------------------
 
     def test_avail(self):
-        os.environ["MOCK_FILE"] = self.path + "data/mock_avail_0.txt"
-        assert True
+        os.environ["MOCK_FILE_AVAIL"] = self.path + "/data/data_avail_simple_pikachu.txt"
+        output = module_avail()
+        assert len(output) == 29
 
-    def test_swap(self):
-        os.environ["MOCK_FILE"] = self.path + "data/mock_swap_0.txt"
-        assert True
+    def test_avail_filtered(self):
+        os.environ["MOCK_FILE_AVAIL"] = self.path + "/data/data_avail_simple_pikachu.txt"
+        output = module_avail(filter_fn=filter_fn_gent_modules)
+        assert len(output) == 15
+        assert list(output) == [
+            "cfd/1.0", "cfd/2.0", "cfd/3.0", "cfd/24", "cfd/", "cfd/5.0",
+            "cfd/2.0afqsdf", "llm/20230627", "llm/20230627", "llm/20230627", "science/",
+            "science/5.3.0", "science/5.3.0", "science/5.3.0", "science/7.2.0"
+        ]
+
+    def test_avail_cluster(self):
+        os.environ["MOCK_FILE_AVAIL_CLUSTER"] = self.path + "/data/data_avail_cluster_simple.txt"
+        output = module_avail(name="cluster/")
+        assert len(output) == 4
+
+    def test_avail_cluster_filtered(self):
+        os.environ["MOCK_FILE_AVAIL_CLUSTER"] = self.path + "/data/data_avail_cluster_simple.txt"
+        output = module_avail(name="cluster/", filter_fn=filter_fn_gent_cluster)
+        assert len(output) == 2
+        assert list(output) == ["cluster/dialga", "cluster/pikachu"]
