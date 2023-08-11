e5b453642974ad071f4460af31f3c6625477a790
Michiel Lachaert <michiellachaert@outlook.com>
2023-08-11 14:59:06 +0200

    fix argument error in lmod_mock script

--- scripts/module_overview/tests/data/lmod_mock.sh
+++ scripts/module_overview/tests/data/lmod_mock.sh
@@ -7,8 +7,8 @@ mod_cmd="$3"
 mod_args="$4"
 
 # Emulated avail command.
-if [ $mod_cmd = "avail" ]; then
-  if [ $mod_args = "cluster/" ]; then
+if [ "$mod_cmd" = "avail" ]; then
+  if [ "$mod_args" = "cluster/" ]; then
     cat "${MOCK_FILE_AVAIL_CLUSTER}" >&2
   else
     cat "${MOCK_FILE_AVAIL}" >&2
@@ -16,12 +16,12 @@ if [ $mod_cmd = "avail" ]; then
 
 
 # Emulated swap command.
-elif [ $mod_cmd = "swap" ]; then
+elif [ "$mod_cmd" = "swap" ]; then
   # extract the cluster name from the 4th argument
-  cluster=$(echo $mod_args | cut -d "/" -f 1)
-  cluster_name=$(echo $mod_args | cut -d "/" -f 2)
+  cluster=$(echo "$mod_args" | cut -d "/" -f 1)
+  cluster_name=$(echo "$mod_args" | cut -d "/" -f 2)
 
-  if [ $cluster = "cluster" ]; then
+  if [ "$cluster" = "cluster" ]; then
     # Substitute CLUSTER by the cluster_name
     echo "${MOCK_FILE_SWAP}" | sed "s/CLUSTER/${cluster_name}/" | xargs cat >&1
   else
