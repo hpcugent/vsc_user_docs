b12962f9dd2d89906e25e4c983d68946f9f3a56c
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2024-07-30 10:28:55 +0200

    Update multi_core.sh

--- intro-HPC/examples/Job-script-examples/multi_core.sh
+++ intro-HPC/examples/Job-script-examples/multi_core.sh
@@ -2,7 +2,7 @@
 #PBS -N mpi_hello             ## job name
 #PBS -l nodes=2:ppn=all       ## 2 nodes, all cores per node
 #PBS -l walltime=2:00:00      ## max. 2h of wall time
-module load intel/2017b
+module load foss/2023a
 module load vsc-mympirun      ## We don't use a version here, this is on purpose
 # go to working directory, compile and run MPI hello world
 cd $PBS_O_WORKDIR
