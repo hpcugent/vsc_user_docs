159aa62af18dd76b5567c00a98a08c16081d9773
EwDa291 <ewout.danneels@ugent.be>
2024-08-09 16:43:09 +0200

    small update, not important

--- scripts/HPC chatbot preprocessor/start_checker.py
+++ scripts/HPC chatbot preprocessor/start_checker.py
@@ -17,3 +17,19 @@ for dirpath, dirnames, filenames in os.walk(directory):
                     lines_until_title += 1
             print(filename + " : " + str(lines_until_title))
     break
+
+directory = "C:\\HPC_werk\\Documentation\\local\\vsc_user_docs\\mkdocs\\docs\\HPC\\linux-tutorial"
+
+for dirpath, dirnames, filenames in os.walk(directory):
+    for filename in filenames:
+        # if filename.endswith("xdmod.md"):
+        #     break
+        if filename.endswith(".md"):
+            lines_until_title = 0
+            with open(directory + "\\" + filename, "r") as file:
+                for line in file:
+                    if line[0] == "#":
+                        break
+                    lines_until_title += 1
+            print(filename + " : " + str(lines_until_title))
+    break
