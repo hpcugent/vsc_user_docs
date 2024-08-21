2e7a00f1b724e77249caef30c62e8aa6c6c9f628
EwDa291 <ewout.danneels@ugent.be>
2024-08-21 17:04:35 +0200

    added test for make_valid_title

---
+++ scripts/HPC_chatbot_preprocessor/tests/test_make_valid_title.py
@@ -0,0 +1,20 @@
+import pytest
+import shutil
+from chatbot_parser import make_valid_title
+
+
+@pytest.mark.parametrize("input_string,expected", [
+    ("", ""),
+    ("A-good-filename-with-dashes", "A-good-filename-with-dashes"),
+    (" A very good filename beginning and ending in a space ", "A very good filename beginning and ending in a space"),
+    ("-A-very-good-filename-beginning-and-ending-in-a-dash-", "A-very-good-filename-beginning-and-ending-in-a-dash"),
+    ("A filename containing bad characters <>:\"/\\|?*\0", "A filename containing bad characters"),
+    ("A filename ending with {some jinja garbage}", "A filename ending with")
+])
+def test_make_valid_title(input_string, expected):
+    assert make_valid_title(input_string) == expected
+
+
+shutil.rmtree("parsed_mds", ignore_errors=True)
+shutil.rmtree("copies", ignore_errors=True)
+shutil.rmtree("if_mangled_files", ignore_errors=True)
