3bdf4c7fb574e567482e31e3684972f3d889f59f
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-08-26 15:43:28 +0200

    rewrite script to only provide activate and deactivate commands

--- scripts/vsc-venv-wrapper/README.md
+++ scripts/vsc-venv-wrapper/README.md
@@ -7,48 +7,38 @@ This avoids multiple issues with the default `venv` command on the HPC.
 One issue is that a virtual environment created for one cluster might not work on another. 
 Additionally, when activating a virtual environment, 
 the same centrally installed modules used during its creation must also be loaded.
-the `vsc-venv` command manages multiple virtual environments for different clusters in a transparent way.
+the `vsc-venv` command manages multiple virtual environments for different clusters in a transparent way, 
+while guaranteeing the same module environment.
 
 ## Usage
 
-A virtual environment can be created by running the following command:
+A virtual environment can be activated by running the following command:
 
 ```bash
-source vsc-venv create [module_script]
+source vsc-venv activate <requirements> [module_script]
 ```
 
-Here, an optional module script can be provided to load the required modules before creating the virtual environment.
+Here, `requirements` is the path to a file containing the python dependencies to install in the virtual environment.
+An optional module script can be provided to load the required modules before creating the virtual environment.
 
-To activate a virtual environment, run the following command:
-
-```bash
-source vsc-venv activate
-```
-
-To install dependencies in a virtual environment, run:
-
-```bash
-vsc-venv install <requirements>
-```
-
-Requirements should be a file containing the dependencies to install.
-At this point, `pip install <package>` can also be used to install, 
-but we recommend using a file like `requirements.txt` to record the packages.
+Automatically, the modules are loaded and the environment is activated. 
+When running this command for the first time, the dependencies from the requirement file are installed.
 
 Now, the software can be run and use packages installed in the virtual environment, along with centrally installed modules.
 To deactivate the virtual environment, run:
 
 ```bash
-deactivate
+source vsc-venv deactivate
 ```
 
-## Example 
+## Example
 
-Suppose the following files are present in the directory:
+Suppose we are on the login node, and we want to make a virtual environment for doduo, which is loaded by default.
+The following files are present in the current directory:
 
 **modules.sh:**
 ```bash
-ml Python/3.7.2-GCCcore-8.2.0
+ml Python/3.12.3-GCCcore-13.3.0
 ```
 
 and **requirements.txt:**
@@ -56,10 +46,37 @@ and **requirements.txt:**
 requests
 ```
 
-We run the following commands to make an environment and install the packages.
+We run the following commands to enter the environment
 
 ```bash
-source vsc-venv create modules.sh
+source vsc-venv activate requirements.txt modules.sh
 ```
 
-This creates a 
+As this creates the virtual environment for the first time, the `venvs` folder is created. 
+Within this folder, a new folder is created for the virtual environment `venv-RHEL8-zen2`.
+
+Now, python 3.12 is loaded and the user can use the `requests` package.
+
+To deactivate the virtual environment, run:
+
+```bash
+source vsc-venv deactivate
+```
+
+### joltik
+
+If we want to create a virtual environment for joltik, we can run the following commands:
+
+```bash
+module swap cluster/joltik
+qsub -I
+source vsc-venv activate requirements.txt modules.sh
+```
+
+the venvs folder now contains two folders:
+
+```bash
+$ ls venvs/
+venv-RHEL8-cascadelake	
+venv-RHEL8-zen2
+```
--- scripts/vsc-venv-wrapper/vsc-venv.sh
+++ scripts/vsc-venv-wrapper/vsc-venv.sh
@@ -1,118 +1,85 @@
-#!/usr/bin/env bash
-
-VENV_NAME="venv-${VSC_OS_LOCAL}-${VSC_ARCH_LOCAL}"
-VENV_LOCATION=$(realpath -m "venvs/$VENV_NAME") # full path of venv
-
 usage() {
-  echo "Usage: $0 {create <optional: modules>|install <requirements>|activate}"
+  echo "Usage: $0 {activate <requirements.txt> [modules.sh] | deactivate}"
   echo ""
   echo "Commands:"
-  echo "  create [modules_script]  Create a virtual environment at $VENV_LOCATION."
-  echo "                           Optionally, load modules from the specified script before creating the environment."
-  echo "  install <requirements>   Install packages from the specified requirements file."
-  echo "                           The virtual environment must be activated before running this command."
-  echo "  activate                 Activate the virtual environment. You need to source this command."
+  echo "  activate <requirements_file> [modules_script] Activate the environment using the specified requirements file."
+  echo "                                                Optionally, load modules from the specified script before activating the environment."
+  echo ""
+  echo "  deactivate                                    Deactivate the virtual environment."
   echo ""
   echo "Example Usage:"
-  echo "  $ $0 create my_modules.sh"
-  echo "  $ source $0 activate"
-  echo "  $ $0 install requirements.txt"
+  echo "  $ source $0 activate requirements.txt modules.sh"
   echo "  $ python my_script.py"
-  echo "  $ deactivate"
-  exit 1
+  echo "  $ $0 deactivate"
+  return 1
 }
-
-# ============================ Main functions ============================
 
 load_modules() {
   local MODULES_SCRIPT="$1" # The modules script to load. Empty if not provided.
 
+
+
+}
+
+# ============================ Main functions ============================
+
+activate() {
+  REQUIREMENTS_FILE="$1"
+  MODULES_SCRIPT="$2"
+
+  VENV_LOCATION=$(realpath -m "venvs/venv-${VSC_OS_LOCAL}-${VSC_ARCH_LOCAL}") # full path of venv
+
+  # === Step 1: Purge Modules === #
   echo "INFO: Purging currently loaded modules. If you want to use modules, provide a modules script."
   module purge
 
-  # If a modules script is provided, try to load it.
-  if [ -n "$MODULES_SCRIPT" ]; then
-    echo "INFO: loading modules from '$MODULES_SCRIPT'"
+  # === Step 2: Load Modules if module script present === #
 
-    # If the module script could not be loaded, exit.
-    if ! source "$MODULES_SCRIPT"; then
-        echo "ERROR: could not load modules from '$MODULES_SCRIPT'"
-        exit 1
-    fi
+  if [ -n "$MODULES_SCRIPT" ]; then
+
+      echo "INFO: loading modules from '$MODULES_SCRIPT'"
+
+      # If the module script could not be loaded, return.
+      if ! source "$MODULES_SCRIPT"; then
+          echo "ERROR: could not load modules from '$MODULES_SCRIPT'"
+          return 1
+  fi
+
+  echo "INFO: Modules loaded successfully"
 
   else
     echo "INFO: No module script provided. Proceeding without extra modules."
   fi
 
-}
+  # === Step 3: Create Virtual Environment if not yet present === #
 
-# === Create ===
-
-create() {
-  local MODULES_SCRIPT="$1" # The modules script to load. Empty if not provided.
-
-  load_modules "$MODULES_SCRIPT"
-
-  # Create a virtual environment
   echo "INFO: creating virtual environment at $VENV_LOCATION"
+  # Will automatically make the venvs folder and venv, does nothing if they already exist
   if ! python -m venv "$VENV_LOCATION"; then
     echo "ERROR: could not create virtual environment"
-    exit 1
+    return 1
   fi
 
-  echo "INFO: virtual environment created"
-}
-
-# === Activate ===
-
-activate() {
-  echo "INFO: Activating..."
-
-  # If the virtual environment does not exist, exit.
-  if [ ! -f "$VENV_LOCATION/bin/activate" ]; then
-    echo "ERROR: virtual environment does not exist. Run 'create' first."
-    exit 1
-  fi
-
-
+  # === Step 4: Activate Virtual Environment === #
 
   source "$VENV_LOCATION/bin/activate"
+
+  # === Step 5: Install Requirements === #
+
+  echo "INFO: installing requirements from '$REQUIREMENTS_FILE'"
+  pip install -r "$REQUIREMENTS_FILE" # This fill finish quickly if the requirements are already installed
+
 }
 
-# === Install ===
-
-install() {
-
-  local REQUIREMENTS="$1"
-  echo "INFO: Installing $REQUIREMENTS..."
-
-  # If no requirements file is provided, exit.
-  if [ ! -f "$REQUIREMENTS" ]; then
-    echo "ERROR: no requirements file provided"
-    exit 1
-  fi
-
-  # The virtual environment must have been activated before running install.
-  # This is equivalent to $VIRTUAL_ENV (which is set by python when activating a venv) being equal to
-  # $VENV_LOCATION (the location of the venv specific to the architecture of the target cluster).
-  if [ "$VIRTUAL_ENV" != "$VENV_LOCATION" ]; then
-        echo "ERROR: virtual environment is not activated. Run 'activate' first."
-        exit 1
-  fi
-
-  # Install the requirements
-  if ! pip install -r "$REQUIREMENTS"; then
-    echo "ERROR: could not install requirements"
-    exit 1
-  fi
-
+deactivate_() {
+  echo "INFO: Deactivating..."
+  deactivate # For now, just use the python `deactivate`
 }
 
 # ============================ Main ============================
 
 case "$1" in
-  create)     create "$2";;
-  install)    install "$2" ;;
-  activate)   activate ;;
-  *)          usage ;;
+  activate)     activate "$2" "$3";;
+  deactivate)  deactivate_ ;;
+  *)            usage ;;
 esac
