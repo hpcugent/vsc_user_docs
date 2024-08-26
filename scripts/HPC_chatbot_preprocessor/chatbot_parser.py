af4832b5a8cd50bc790353a232fcca5e51e35e90
EwDa291 <ewout.danneels@ugent.be>
2024-08-26 15:08:37 +0200

    smal fix

--- scripts/HPC_chatbot_preprocessor/chatbot_parser.py
+++ scripts/HPC_chatbot_preprocessor/chatbot_parser.py
@@ -825,7 +825,8 @@ def split_and_write_os_specific_section(text, metadata, subtitle_order, title_or
 
         # add first subtitle in front of section again
         if len(jinja_text) != 0:
-            jinja_text = "#" * metadata[TITLE_DEPTH] + " " + metadata[SUBTITLE] + "\n" + jinja_text
+            if options[SPLIT_ON_TITLES]:
+                jinja_text = "#" * metadata[TITLE_DEPTH] + " " + metadata[SUBTITLE] + "\n" + jinja_text
 
             # re-adjust text to correct overcorrections
             jinja_text = re.sub('"' + OS + '"', OS, jinja_text)
@@ -951,7 +952,7 @@ def main(options):
 
         # create directories for the source markdown file
         for directory in [root_dir_generic, os.path.join(PARSED_MDS, OS_SPECIFIC_DIR), root_dir_os_specific_linux, root_dir_os_specific_windows, root_dir_os_specific_macos, os.path.join(root_dir_generic, curr_dirs[0]), os.path.join(root_dir_os_specific_linux, curr_dirs[0]), os.path.join(root_dir_os_specific_windows, curr_dirs[0]), os.path.join(root_dir_os_specific_macos, curr_dirs[0])]:
-            os.makedirs(os.path.join(options[DESTINATION_DIRECTORY], directory), exist_ok=True)
+            os.makedirs(directory, exist_ok=True)
 
         # process the jinja macros
         jinja_parser(filename, copy_file, options)
