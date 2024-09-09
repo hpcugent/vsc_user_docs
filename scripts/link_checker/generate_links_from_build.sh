182b9e2e46c209e9432a3c2e9a39c34067848b45
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-09-09 18:43:56 +0200

    add script that generates the links from the build

---
+++ scripts/link_checker/generate_links_from_build.sh
@@ -0,0 +1,17 @@
+main() {
+  local build_dir=$1
+
+  local a_tag_regex='<a\s+[^>]*href="http[^"]*'
+
+  # Generate links from build:
+  # 1. Find all <a> tags in build, H option to print filename (/path/to/file:<a href="url)
+  # 2. convert middle part to space (/path/to/file url)
+  # 3. sort by url
+  # 4. remove duplicates (only based on url)
+  grep -rHoP "$a_tag_regex" "$build_dir" \
+      | sed 's/:<a.*href="/ /' \
+      | sort -k2,2 \
+      | uniq -f1
+}
+
+main "$1"
