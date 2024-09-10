392848d6b54c6aff64c50119e034ddc03bc6204b
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-09-10 09:45:08 +0200

    remove set -x

--- scripts/modules_auto_pr/modules_auto_pr.sh
+++ scripts/modules_auto_pr/modules_auto_pr.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e  # Exit immediately if a command exits with a non-zero status
-set -x  # Print commands and their arguments as they are executed
 set -u  # Treat unset variables as an error when substituting
 
 ####################################################################################################
